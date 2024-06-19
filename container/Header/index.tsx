dc08f3b01b1d3a939a82c2b12d9fbfa6869aa99a
Sonke Gumede <sonke.gumede@sovtech.com>
2024-06-19 23:48:11 +0200

    fix: update header

--- container/Header/index.tsx
+++ container/Header/index.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import { View, TouchableOpacity } from 'react-native';
-import Ionicons from '@expo/vector-icons/Ionicons'
-import EvilIcons from '@expo/vector-icons/EvilIcons'
+import Ionicons from '@expo/vector-icons/Ionicons';
+import EvilIcons from '@expo/vector-icons/EvilIcons';
+import { styled } from 'styled-components/native';
+
+const Container = styled.View`
+  display: flex;
+  background-color: ${props => props.theme.colors.primary.dark};
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center; 
+  padding: 10px;
+`
 
 const Header = ({ navigation }) => {
   return (
-    <View style={{ display: 'flex', backgroundColor: 'red',flexDirection: 'row', justifyContent: 'space-between', alignItems:'center' }}>
+    <Container>
       <TouchableOpacity onPress={() => navigation.navigate('profile')}>
-        <EvilIcons name="user" color="black" size={50} />
+        <EvilIcons name="user" color="white" size={30} />
       </TouchableOpacity>
       <TouchableOpacity onPress={() => navigation.navigate('notifications')}>
-        <Ionicons name="notifications-outline" color="black" size={50}/>
+        <Ionicons name="notifications-outline" color="white" size={30} />
       </TouchableOpacity>
-    </View>
+    </Container>
   );
 };
 
