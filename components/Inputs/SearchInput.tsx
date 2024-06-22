de7763afc8420a0c55053927007daa76517eb7d3
Sonke Gumede <sonke.gumede@sovtech.com>
2024-06-22 16:13:32 +0200

    fix: delete unused components

--- components/Inputs/SearchInput.tsx
+++ components/Inputs/SearchInput.tsx
@@ -1,23 +1,64 @@
 import React from "react";
 import { SearchBar, SearchBarProps, makeStyles } from '@rneui/themed';
+import { View, Text } from "react-native";
 
-const useStyles = makeStyles((theme) => ({
-  container: {
+const useSarchBarStyle = makeStyles((theme) => ({
+  inputContainerStyle: {
     backgroundColor: theme.colors.primary.dark,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    padding: 10,
+    borderColor: theme.colors.primary.white,
+    borderStyle: 'solid',
+    borderBottomWidth: 1,
+    borderLeftWidth:1,
+    borderTopWidth: 1,
+    borderRightWidth: 1,
+    borderRadius: 10,
+    padding: 0
+  },
+  containerStyle:{
+    borderBottomWidth: 0,
+    borderTopWidth: 0,
+    padding: 0,
+    width: '100%'
+  },
+  searchIcon: {
+    color: theme.colors.primary.white
+  },
+  inputStyle: {
+    borderWidth: 0
   }
 }));
 
-const SearchInput:React.FunctionComponent<SearchBarProps> = ()=>{
-  const styles = useStyles();
+const useRNStyle = makeStyles(({colors, fonts})=>({
+  container: {
+    flex: 1,
+    justifyContent: "flex-start",
+    flexDirection: 'column',
+    width: '100%'
+  },
+  header: {
+    color: colors.primary.gold,
+    fontSize: fonts.size.large,
+    fontWeight: '400',
+    marginBottom: 5
+  }
+}))
+
+const SearchInput: React.FunctionComponent<SearchBarProps> = () => {
+  const searchBarStyles = useSarchBarStyle();
+  const rnStyle = useRNStyle();
   return (
+    <View style={rnStyle.container}>
+    <Text style={rnStyle.header}>What song will inspire you today</Text>
     <SearchBar
+      inputStyle={searchBarStyles.inputStyle}
+      containerStyle={searchBarStyles.containerStyle}
+      searchIcon={searchBarStyles.searchIcon}
+      inputContainerStyle={searchBarStyles.inputContainerStyle}
+      placeholderTextColor="white"
+      placeholder="Search"
       
-    
     />
+    </View>
   )
 }
 
